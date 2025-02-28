8653ffec27e3b4daba2bffaf5fd510efdcca9b85
smarkandu <s_markan@hotmail.com>
2025-02-27 23:00:21 -0500

    feat #12, #35: Implemented Issue Order for deploy using Command Pattern

--- src/main/java/ca/concordia/soen6441/project/interfaces/Player.java
+++ src/main/java/ca/concordia/soen6441/project/interfaces/Player.java
@@ -9,10 +9,8 @@ public interface Player {
     Order next_order();
     String getName();
     int getTotalNumberOfReinforcementsPerTurn();
-
     void assignCountry(Country p_country);
-
     int getReinforcements();
-
     void setReinforcements(int p_reinforcements);
+    int getNumberOfTroopsOrderedToDeploy();
 }
