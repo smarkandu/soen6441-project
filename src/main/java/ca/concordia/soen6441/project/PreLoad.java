3db7ff852a6280af13fce9651a74ffe0aa117b2b
smarkandu <s_markan@hotmail.com>
2025-03-02 02:14:26 -0500

    loadMap enabled; resetMap function added

--- src/main/java/ca/concordia/soen6441/project/PreLoad.java
+++ src/main/java/ca/concordia/soen6441/project/PreLoad.java
@@ -6,12 +6,10 @@ public class PreLoad extends Edit {
     }
 
     public void loadMap(String p_filename) {
-        System.out.println("map has been loaded");
-
-        // TODO: Shubo Uncomment below (feature #3)
-//        MapFileReader mapFileReader = new MapFileReader();
-//        mapFileReader.readMapFile(p_filename, d_gameEngine);
-//        d_gameEngine.setPhase(new PostLoad(d_gameEngine));
+        d_gameEngine.resetMap();
+        MapFileReader mapFileReader = new MapFileReader();
+        mapFileReader.readMapFile(p_filename, d_gameEngine);
+        d_gameEngine.setPhase(new PostLoad(d_gameEngine));
     }
 
     public void saveMap(String p_filename) {
