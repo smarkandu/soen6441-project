f0ccc376b77fd702f90819e1b5f68114b23c04d0
Shubo <babpudebnath@gmail.com>
2025-03-07 05:05:58 -0500

    Two new methods added to ValidateMapImpl and test cases added to the new ValidateMapTest class

--- src/main/java/ca/concordia/soen6441/project/map/ValidateMapImpl.java
+++ src/main/java/ca/concordia/soen6441/project/map/ValidateMapImpl.java
@@ -1,11 +1,10 @@
-package ca.concordia.soen6441.project.map;
+package ca.concordia.soen6441.project;
 
 import ca.concordia.soen6441.project.interfaces.Continent;
 import ca.concordia.soen6441.project.interfaces.Country;
 import ca.concordia.soen6441.project.interfaces.ValidateMap;
 
 import java.util.*;
-
 public class ValidateMapImpl implements ValidateMap {
 
     private SortedMap<String, Continent> d_Continents;
@@ -19,6 +18,16 @@ public class ValidateMapImpl implements ValidateMap {
     }
 
     public boolean isMapValid() {
+        // NEW - Validate that every neighbor exists
+        if (!validateNeighborExistence()) {
+            return false;
+        }
+        // NEW - Validate that neighbor relationships are bidirectional
+        // So, if Country A lists Country B as a neighbor, then Country B must also list Country A as a neighbor.
+        if (!validateBidirectionalNeighbors()) {
+            return false;
+        }
+
         if (!isGraphConnected()) {
             return false;
         }
@@ -28,6 +37,37 @@ public class ValidateMapImpl implements ValidateMap {
         }
 
         System.out.println("Map is valid.");
+        return true;
+    }
+
+    // NEW - Validate that every neighbor in a country's neighbor list exists in d_Countries.
+    private boolean validateNeighborExistence() {
+        for (Country country : d_Countries.values()) {
+            for (String l_neighborID : country.getNeighborIDs()) {
+                if (!d_Countries.containsKey(l_neighborID)) {
+                    System.out.println("Invalid map: Neighbor " + l_neighborID
+                            + " for the country " + country.getID() + " does not exist in d_Countries database.");
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // NEW - Validate that neighbor relationships are bidirectional.
+    private boolean validateBidirectionalNeighbors() {
+        for (Country country : d_Countries.values()) {
+            for (String neighborID : country.getNeighborIDs()) {
+                Country neighborCountry = d_Countries.get(neighborID);
+                // Check if the neighbor's list includes the original country.
+                if (!neighborCountry.getNeighborIDs().contains(country.getID())) {
+                    System.out.println("Invalid map: Bidirectional neighbor missing. Country "
+                            + country.getID() + " lists " + neighborID
+                            + " as neighbor, but not vice versa.");
+                    return false;
+                }
+            }
+        }
         return true;
     }
 
@@ -59,7 +99,6 @@ public class ValidateMapImpl implements ValidateMap {
         p_visited.add(p_countryID);
         Country l_current = d_Countries.get(p_countryID);
 
-
         for (String l_neighborID : l_current.getNeighborIDs()) {
             if (d_Countries.containsKey(l_neighborID)) {
                 exploreCountries(l_neighborID, p_visited);
@@ -76,7 +115,8 @@ public class ValidateMapImpl implements ValidateMap {
             String l_continentID = l_country.getContinent().getID();
             // Check if the country's continent actually exists
             if (!d_Continents.containsKey(l_continentID)) {
-                System.out.println("Country belongs to a non-existent continent");
+                System.out.println("Invalid map: Country " + l_country.getID()
+                        + " belongs to a non-existent continent " + l_continentID + ".");
                 return false;
             }
 
@@ -84,13 +124,13 @@ public class ValidateMapImpl implements ValidateMap {
                 l_continentToCountries.put(l_continentID, new HashSet<>());
             }
             l_continentToCountries.get(l_continentID).add(l_country.getID());
-
         }
 
         // Check every continent has at least one country
         for (Continent l_continent : d_Continents.values()) {
             if (!l_continentToCountries.containsKey(l_continent.getID()) || l_continentToCountries.get(l_continent.getID()).isEmpty()) {
-                System.out.println("No countries associated with this continent");
+                System.out.println("Invalid map: No countries associated with continent "
+                        + l_continent.getID() + ".");
                 return false;
             }
         }
@@ -100,7 +140,6 @@ public class ValidateMapImpl implements ValidateMap {
             System.out.println("There is one country belonging to more than one continent");
             return false;
         }
-
 
         return true;
     }
@@ -113,12 +152,15 @@ public class ValidateMapImpl implements ValidateMap {
                 if(l_continent.getNumericID() == l_country.getContinent().getNumericID()){
                     l_numberOfContinentsCountryBelongTo++;
                     if(l_numberOfContinentsCountryBelongTo > 1) {
+                        System.out.println("Invalid map: Country " + l_country.getID()
+                                + " belongs to more than one continent.");
                         return false;
                     }
                 }
             }
 
         }
+
         return true;
     }
 }
