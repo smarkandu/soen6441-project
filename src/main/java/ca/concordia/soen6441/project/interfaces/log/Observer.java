8c9fd33b09db69c7f2f37a1aeced217fe581eae2
marcchicoye <marcchicoye@gmail.com>
2025-03-22 06:50:05 -0400

    Refactoring

--- src/main/java/ca/concordia/soen6441/project/interfaces/log/Observer.java
+++ src/main/java/ca/concordia/soen6441/project/interfaces/log/Observer.java
@@ -4,5 +4,5 @@ package ca.concordia.soen6441.project.interfaces.log;
 import ca.concordia.soen6441.project.log.Observable;
 
 public interface Observer {
-    public void update(Observable p_o);
+    void update(Observable p_o);
 }
