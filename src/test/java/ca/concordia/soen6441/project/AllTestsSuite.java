0da0da8d8c224f84ec93e20a06fbec2851cceae2
marcchicoye <marcchicoye@gmail.com>
2025-03-28 22:09:29 -0400

    Added more test units

--- src/test/java/ca/concordia/soen6441/project/AllTestsSuite.java
+++ src/test/java/ca/concordia/soen6441/project/AllTestsSuite.java
@@ -1,12 +1,8 @@
 package ca.concordia.soen6441.project;
 
 import ca.concordia.soen6441.project.gameplay.CountryAssignmentTest;
-import ca.concordia.soen6441.project.gameplay.orders.AdvanceTest;
-import ca.concordia.soen6441.project.gameplay.orders.AirliftTest;
-import ca.concordia.soen6441.project.gameplay.orders.Blockade;
-import ca.concordia.soen6441.project.gameplay.orders.BlockadeTest;
-import ca.concordia.soen6441.project.map.ValidateMapFromFile;
-import ca.concordia.soen6441.project.map.ValidateMapImplTest;
+import ca.concordia.soen6441.project.gameplay.orders.*;
+import ca.concordia.soen6441.project.map.*;
 import ca.concordia.soen6441.project.phases.AssignReinforcementsTest;
 import ca.concordia.soen6441.project.phases.IssueOrderTest;
 import ca.concordia.soen6441.project.phases.OrderExecutionTest;
@@ -19,17 +15,21 @@ import org.junit.platform.suite.api.Suite;
 @IncludeEngines("junit-jupiter")
 //@SelectPackages("com.example.tests") // Replace with your test package
 @SelectClasses({
-        GameEngineTest.class,
-        StartupTest.class,
-        OrderExecutionTest.class,
-        IssueOrderTest.class,
-        AssignReinforcementsTest.class,
-        ValidateMapImplTest.class,
-
-        CountryAssignmentTest.class,
-        BlockadeTest.class,
+        AdvanceTest.class,
         AirliftTest.class,
-        AdvanceTest.class
+        BlockadeTest.class,
+        BombTest.class,
+        CountryAssignmentTest.class,
+        ContinentImplTest.class,
+        CountryImplTest.class,
+        MapFileReaderTest.class,
+        ValidateMapFromFile.class,
+        ValidateMapImplTest.class,
+        AssignReinforcementsTest.class,
+        IssueOrderTest.class,
+        OrderExecutionTest.class,
+        StartupTest.class,
+        GameEngineTest.class,
 })
 
 public class AllTestsSuite {
