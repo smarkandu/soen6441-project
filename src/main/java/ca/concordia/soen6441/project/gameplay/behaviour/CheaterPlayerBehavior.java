da02239522c69791797e567a586cc47f3fcca03d
smarkandu <s_markan@hotmail.com>
2025-04-01 10:32:40 -0400

    Added abstract class ComputerPlayerBehavior which almost all the concrete player behavior classes derive from

--- src/main/java/ca/concordia/soen6441/project/gameplay/behaviour/CheaterPlayerBehavior.java
+++ src/main/java/ca/concordia/soen6441/project/gameplay/behaviour/CheaterPlayerBehavior.java
@@ -1,21 +1,28 @@
 package ca.concordia.soen6441.project.gameplay.behaviour;
 
-import ca.concordia.soen6441.project.interfaces.Order;
-import ca.concordia.soen6441.project.interfaces.Player;
-import ca.concordia.soen6441.project.interfaces.gameplay.behavior.PlayerBehavior;
-
 /**
  * Class which implements the Strategy design pattern for the Cheater player behavior
  */
-public class CheaterPlayerBehavior implements PlayerBehavior {
+public class CheaterPlayerBehavior extends ComputerPlayerBehavior {
     /**
      * {@inheritDoc}
      */
     @Override
-    public void issue_order(Player p_player, Order p_order) {
+    public void deployment() {
 
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void attackTransfer() {
+
+    }
+
+    /**
+     * String representing the object
+     */
     @Override
     public String toString() {
         return "Cheater";
