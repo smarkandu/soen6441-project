0df73c4f86df045877efb5ec5f59dc4fac1bc7e5
smarkandu <s_markan@hotmail.com>
2025-04-04 18:16:31 -0400

    feat #126: Write the results of the computer player into the log and screen at the end of each issueOrder phase

--- src/main/java/ca/concordia/soen6441/project/gameplay/behaviour/ComputerPlayerBehavior.java
+++ src/main/java/ca/concordia/soen6441/project/gameplay/behaviour/ComputerPlayerBehavior.java
@@ -1,7 +1,9 @@
 package ca.concordia.soen6441.project.gameplay.behaviour;
 
+import ca.concordia.soen6441.project.GameDriver;
 import ca.concordia.soen6441.project.interfaces.Player;
 import ca.concordia.soen6441.project.interfaces.gameplay.behavior.PlayerBehavior;
+import ca.concordia.soen6441.project.log.LogEntryBuffer;
 
 import java.io.Serializable;
 
@@ -13,17 +15,20 @@ public abstract class ComputerPlayerBehavior implements PlayerBehavior, Serializ
     public void issue_order(Player p_player) {
         deployment(p_player);
         attackTransfer(p_player);
+        LogEntryBuffer.getInstance().appendToBuffer(GameDriver.getGameEngine().getPhase().getPhaseName(), true);
         p_player.getPlayerManager().getGameEngine().getPhase().next();
     }
 
     /**
      * Issue deployment orders
+     *
      * @param p_player The player to whom the order corresponds to
      */
     public abstract void deployment(Player p_player);
 
     /**
      * Issue attack / transfer orders
+     *
      * @param p_player The player to whom the order corresponds to
      */
     public abstract void attackTransfer(Player p_player);
