488acbc962e41febce6f2c705055559e6e38a137
smarkandu <s_markan@hotmail.com>
2025-04-05 00:47:52 -0400

    Bug Fix: import for LogEntryBuffer re-added

--- src/main/java/ca/concordia/soen6441/project/gameplay/behaviour/ComputerPlayerBehavior.java
+++ src/main/java/ca/concordia/soen6441/project/gameplay/behaviour/ComputerPlayerBehavior.java
@@ -3,15 +3,18 @@ package ca.concordia.soen6441.project.gameplay.behaviour;
 import ca.concordia.soen6441.project.GameDriver;
 import ca.concordia.soen6441.project.interfaces.Player;
 import ca.concordia.soen6441.project.interfaces.gameplay.behavior.PlayerBehavior;
+import ca.concordia.soen6441.project.log.LogEntryBuffer;
 
 import java.io.Serializable;
 
-public abstract class ComputerPlayerBehavior implements PlayerBehavior, Serializable {
+public abstract class ComputerPlayerBehavior implements PlayerBehavior, Serializable
+{
     /**
      * {@inheritDoc}
      */
     @Override
-    public void issue_order(Player p_player) {
+    public void issue_order(Player p_player)
+    {
         deployment(p_player);
         attackTransfer(p_player);
         LogEntryBuffer.getInstance().appendToBuffer(GameDriver.getGameEngine().getPhase().getPhaseName(), true);
@@ -20,12 +23,14 @@ public abstract class ComputerPlayerBehavior implements PlayerBehavior, Serializ
 
     /**
      * Issue deployment orders
+     *
      * @param p_player The player to whom the order corresponds to
      */
     public abstract void deployment(Player p_player);
 
     /**
      * Issue attack / transfer orders
+     *
      * @param p_player The player to whom the order corresponds to
      */
     public abstract void attackTransfer(Player p_player);
